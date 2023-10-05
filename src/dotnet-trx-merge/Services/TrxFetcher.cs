e0433bfac35ef43fcacbb1f278157d1512b2dbce
Sam Holder <sam.holder@coverwise.com>
2023-10-05 09:54:53 +0100

    feat: deal with duplicated test ids in trx files

--- src/dotnet-trx-merge/Services/TrxFetcher.cs
+++ src/dotnet-trx-merge/Services/TrxFetcher.cs
@@ -17,9 +17,9 @@ public class TrxFetcher : ITrxFetcher
 
     private void FetchOnlyLatest(XDocument mergedDocument, string[] filesToMerge)
     {
-        var testResultDictionary = new Dictionary<string, XElement>();
+        var testResultDictionary = new Dictionary<TestIdentity, XElement>();
         var testDefinitionsDictionary = new Dictionary<string, XElement>();
-        var testEntriesDictionary = new Dictionary<string, XElement>();
+        var testEntriesDictionary = new Dictionary<TestIdentity, XElement>();
         var outcomeDictionary = new Dictionary<string, int>();
         foreach (var trxFile in filesToMerge)
         {
@@ -42,19 +42,19 @@ public class TrxFetcher : ITrxFetcher
             _log.Debug($"Found {results?.Count()} tests in file {trxFile}");
             foreach (var unitTestResult in results)
             {
-                var testId = unitTestResult.Attribute("testId")!.Value;
+                TestIdentities testId = new TestIdentities(unitTestResult.Attribute("testId")!.Value, unitTestResult.Attribute("testName")!.Value, unitTestResult.Attribute("executionId")!.Value);
                 var endTime = DateTime.Parse(unitTestResult.Attribute("endTime")?.Value!);
 
-                if (!testResultDictionary.ContainsKey(testId) || DateTime.Parse(testResultDictionary[testId].Attribute("endTime")!.Value) < endTime)
+                if (!testResultDictionary.ContainsKey(testId.TestIdentity) || DateTime.Parse(testResultDictionary[testId.TestIdentity].Attribute("endTime")!.Value) < endTime)
                 {
-                    if (testResultDictionary.ContainsKey(testId))
-                        outcomeDictionary[testResultDictionary[testId].Attribute("outcome")!.Value.ToLower()]--;
+                    if (testResultDictionary.ContainsKey(testId.TestIdentity))
+                        outcomeDictionary[testResultDictionary[testId.TestIdentity].Attribute("outcome")!.Value.ToLower()]--;
                     
-                    testResultDictionary[testId] = unitTestResult;
-                    testDefinitionsDictionary[testId] = GetTestDefinition(definitions, testId);
-                    testEntriesDictionary[testId] = GetTestEntry(entries, testId);
-                    
-                    var outcomeValue = testResultDictionary[testId].Attribute("outcome")!.Value.ToLower();
+                    testResultDictionary[testId.TestIdentity] = unitTestResult;
+                    testDefinitionsDictionary[testId.TestIdentity.TestId] = GetTestDefinition(definitions, testId.TestIdentity.TestId);
+                    testEntriesDictionary[testId.TestIdentity] = GetTestEntry(entries, testId.TestEntryId);
+
+                    var outcomeValue = testResultDictionary[testId.TestIdentity].Attribute("outcome")!.Value.ToLower();
                     
                     if(!outcomeDictionary.ContainsKey(outcomeValue!))
                         outcomeDictionary[outcomeValue] = 0;
@@ -128,6 +128,6 @@ public class TrxFetcher : ITrxFetcher
     private XElement GetTestDefinition(IEnumerable<XElement> definitions, string testId)
         => definitions.SingleOrDefault(entry => entry.Attribute("id")!.Value.Equals(testId))!;
 
-    private XElement GetTestEntry(IEnumerable<XElement> entries, string testId)
-        => entries.SingleOrDefault(entry => entry.Attribute("testId")!.Value.Equals(testId))!;
+    private XElement GetTestEntry(IEnumerable<XElement> entries, TestEntry testEntryId)
+        => entries.SingleOrDefault(entry => new TestEntry(entry.Attribute("testId")!.Value, entry.Attribute("executionId")!.Value).Equals(testEntryId))!;
 }
--- test/dotnet-trx-merge.UnitTests/Services/TrxFetcherTests.cs
+++ test/dotnet-trx-merge.UnitTests/Services/TrxFetcherTests.cs
@@ -13,7 +13,10 @@ public class TrxFetcherTests
     private const string FirstFilePath = "../../../Fixtures/Merge/TrxAllPass.trx";
     private const string SecondFilePath = "../../../Fixtures/Merge/TrxWithFailures.trx";
     private const string ThirdFilePath = "../../../Fixtures/Merge/TrxAllPassSecondRun.trx";
-    
+    private const string FirstDuplicateFilePath = "../../../Fixtures/Merge/DuplicateTestIdAllPass.trx";
+    private const string SecondDuplicateFilePath = "../../../Fixtures/Merge/TrxWithFailures.trx";
+    private const string ThirdDupliacteFilePath = "../../../Fixtures/Merge/DuplicateTestIdAllPassSecondRun.trx";
+
     [Fact]
     public void AddLatestTests_WithOneFile_AllPass()
     {
@@ -237,6 +240,118 @@ public class TrxFetcherTests
             "86e2b6e4-df7a-e4fa-006e-c056c908e219");
         
         ValidateOutcome(mergedDocument, 1, 1, 0, "Completed");
+    }
+
+    [Fact]
+    public void AddLatestTests_WithMultipleTestsWithSameId_SingleFileMerge_AllTestsAreOutput()
+    {
+        // Arrange
+        var logger = new Logger();
+        var trxFetcher = new TrxFetcher(logger);
+        var mergedDocument = new XDocument(new XElement("TestRun"));
+
+        // Act
+        trxFetcher.AddLatestTests(mergedDocument, new[] { FirstDuplicateFilePath });
+
+        // Assert
+        var results = mergedDocument.Descendants("UnitTestResult");
+        results.Should().HaveCount(3);
+        ValidateTestResult(results.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d68813f",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test1",
+            "Passed");
+
+        ValidateTestResult(results.ElementAt(1),
+            "0213c00a-c8f7-4787-a5a0-179d076d5c85",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test2",
+            "Passed");
+
+        ValidateTestResult(results.ElementAt(2),
+            "f72ee21c-4b63-4e22-b839-444caab919cd",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test3",
+            "Passed");
+
+        var definitions = mergedDocument.Descendants("UnitTest");
+        definitions.Should().HaveCount(1);
+        ValidateTestDefinition(definitions.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d68813f",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "ShouldCheckoutEachScenarioSuccessfully");
+
+        var entries = mergedDocument.Descendants("TestEntry");
+        entries.Should().HaveCount(3);
+        ValidateTestEntry(entries.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d68813f",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateTestEntry(entries.ElementAt(1),
+            "0213c00a-c8f7-4787-a5a0-179d076d5c85",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateTestEntry(entries.ElementAt(2),
+            "f72ee21c-4b63-4e22-b839-444caab919cd",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateOutcome(mergedDocument, 3, 3, 0, "Completed");
+    }
+
+    [Fact]
+    public void AddLatestTests_WithMultipleTestsWithSameId_ShouldUseTheLatestExecutionId()
+    {
+        // Arrange
+        var logger = new Logger();
+        var trxFetcher = new TrxFetcher(logger);
+        var mergedDocument = new XDocument(new XElement("TestRun"));
+
+        // Act
+        trxFetcher.AddLatestTests(mergedDocument, new[] { FirstDuplicateFilePath, ThirdDupliacteFilePath });
+
+        // Assert
+        var results = mergedDocument.Descendants("UnitTestResult");
+        results.Should().HaveCount(3);
+        ValidateTestResult(results.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d688131",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test1",
+            "Passed");
+
+        ValidateTestResult(results.ElementAt(1),
+            "0213c00a-c8f7-4787-a5a0-179d076d5c81",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test2",
+            "Passed");
+
+        ValidateTestResult(results.ElementAt(2),
+            "f72ee21c-4b63-4e22-b839-444caab919c1",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "Test3",
+            "Passed");
+
+        var definitions = mergedDocument.Descendants("UnitTest");
+        definitions.Should().HaveCount(1);
+        ValidateTestDefinition(definitions.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d68813f",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5",
+            "ShouldCheckoutEachScenarioSuccessfully");
+
+        var entries = mergedDocument.Descendants("TestEntry");
+        entries.Should().HaveCount(3);
+        ValidateTestEntry(entries.ElementAt(0),
+            "ac62796d-afd7-40db-a5a2-14798d688131",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateTestEntry(entries.ElementAt(1),
+            "0213c00a-c8f7-4787-a5a0-179d076d5c81",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateTestEntry(entries.ElementAt(2),
+            "f72ee21c-4b63-4e22-b839-444caab919c1",
+            "bd0f3e87-2d49-7223-2c00-e0924f9c83f5");
+
+        ValidateOutcome(mergedDocument, 3, 3, 0, "Completed");
     }
 
     private static void ValidateTestResult(XElement testResult,
