eee8d0f6b6df060aada9f7112c8facfd439e9b72
Sam Holder <sam.holder@coverwise.com>
2023-10-05 09:54:53 +0100

    feat: deal with duplicated test ids in trx files

---
+++ src/dotnet-trx-merge/Services/TestIdentities.cs
@@ -0,0 +1,45 @@
+﻿namespace dotnet_trx_merge.Services
+{
+    internal record TestIdentities
+    {
+        public TestIdentities(string testId, string testName, string executionId)
+        {
+            TestIdentity = new TestIdentity(testId, testName);
+            TestEntryId = new TestEntry(testId, executionId);
+        }
+
+        public TestIdentity TestIdentity { get; }
+        public TestEntry TestEntryId { get; }
+
+        public override string? ToString()
+        {
+            return $"{TestIdentity.TestId} - {TestEntryId.ExecutionId}";
+        }
+    }
+
+    internal record TestIdentity
+    {
+        public TestIdentity(string testId, string testName)
+        {
+            TestId = testId;
+            TestName = testName;
+        }
+
+        public string TestId { get; }
+        public string TestName { get; }
+    }
+
+
+
+    internal record TestEntry
+    {
+        public TestEntry(string testId, string executionId)
+        {
+            TestId = testId;
+            ExecutionId = executionId;
+        }
+
+        public string TestId { get; }
+        public string ExecutionId { get; }
+    }
+}
